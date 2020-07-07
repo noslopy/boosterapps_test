45956785d86775c0ad6c32d79f80404cc099f325
noslopy <info@noslopy.dev>
2020-07-07 13:21:53 +0200

    ✅ add to cart optin test

--- lib/test_manager/fb_app_tests.rb
+++ lib/test_manager/fb_app_tests.rb
@@ -19,4 +19,41 @@ module FbAppTests
     @result.all?{|_,v| v}
   end
 
+  def check_atc_optin_window? url:
+    @result = {}
+    navigate_to_atc_optin(url)
+    @result['optin_window_rendered'] =
+      !find('.ba-fb-add-tc-popup__container').inspect.strip.empty?
+    @result['send_to_messenger_button_rendered'] =
+      page.find('div', class: 'fb-send-to-messenger')['fb-xfbml-state'] == 'rendered'
+    @result.all?{|_,v| v}
+  end
+
+  def check_atc_optin_ignored_behaviour? url:
+    @result = {}
+    navigate_to_atc_optin(url)
+    @result['optin_window_rendered'] =
+      !find('.ba-fb-add-tc-popup__container').inspect.strip.empty?
+    find(".ba-fb-add-tc-popup__close", match: :first)
+    find('button', class: 'ba-fb-add-tc-popup__close').click
+    find(".cart", match: :first)
+    @result['navigates_to_cart_if closed_by_x'] = current_url.include? 'cart'
+    navigate_to_atc_optin(url)
+    find(".ba-fb-add-tc-pop-footer", match: :first)
+    find('small', class: 'ba-fb-add-tc-pop-footer').click
+    find(".cart", match: :first)
+    @result['navigates_to_cart_if closed_by_text'] = current_url.include? 'cart'
+    @result.all?{|_,v| v}
+  end
+
+  private
+
+  def navigate_to_atc_optin url
+    cleanup!
+    visit(url)
+    find(".ba-modal-close", match: :first).click
+    find("#AddToCart-product-template", match: :first).click
+    find(".ba-fb-add-tc-popup__container", match: :first)
+  end
+
 end
